a393055e661b3f9ee4355e3508caafb2dda55ef4
Gabriel Heiler <gabrielheiler@gmail.com>
2018-06-14 22:52:23 +0200

    last cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,27 @@ import { NgxPermissionsService } from 'ngx-permissions';
 import { environment } from './../environments/environment';
 import * as log from 'loglevel';
 import { User } from './appcommon/models/user';
-import { routerTransition } from './components/animations/router.animations';
 import { UserInfoService } from './appcommon/services/user-info.service';
 
 @Component({
-  selector: 'app-root',
-  animations: [routerTransition],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private permsService: NgxPermissionsService, private userService: UserInfoService) {}
+    constructor(private permsService: NgxPermissionsService, private userService: UserInfoService) {}
 
-  ngOnInit(): void {
-    log.setLevel(environment.LOG_LEVEL);
-    log.setDefaultLevel(environment.LOG_LEVEL);
+    ngOnInit(): void {
+        log.setLevel(environment.LOG_LEVEL);
+        log.setDefaultLevel(environment.LOG_LEVEL);
 
-    this.userService.getUserInfo().subscribe((user: User) => {
-      user.permissions.forEach(perm => {
-        this.permsService.addPermission(perm);
-      });
-    });
-  }
-  getState(outlet) {
-    return outlet.activatedRouteData.state;
-  }
+        this.userService.getUserInfo().subscribe((user: User) => {
+            user.permissions.forEach(perm => {
+                this.permsService.addPermission(perm);
+            });
+        });
+    }
+    getState(outlet) {
+        return outlet.activatedRouteData.state;
+    }
 }
